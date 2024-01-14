8098782d9ed9f0d9a4538738122d41921b151be2
Kjanoudii <janoudikhaled@gmail.com>
2024-01-14 14:37:06 +0200

    Firebase Data pulled

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,27 +1,28 @@
-
-
 export default function List(prop) {
-    const { setListName, name, index, setIndex, changeItems } = prop
-    const getListName = () => {
-        setListName(name);
-        console.log(name);
-    };
+  const { setListName, name, index, setIndex, changeItems } = prop;
+  const getListName = () => {
+    setListName(name);
+    console.log(name);
+  };
 
-    const handleClick = () => {
-        setIndex(index)
-        changeItems(index)
+  const handleClick = () => {
+    setIndex(index);
+    changeItems(index);
 
-        console.log(index)
+    console.log(index);
+  };
 
-
-    }
-
-    return (
-        <div className="cursor-pointer border-b border-white border-opacity-70 rounded py-2 hover:bg-gray-100">
-            <h3 className=" py-2 text-center" onClick={() => { getListName(); handleClick() }} >
-                {name}
-            </h3>
-        </div>
-    );
+  return (
+    <div className="cursor-pointer border-b border-white border-opacity-70 rounded py-2 hover:bg-gray-100">
+      <h3
+        className=" py-2 text-center"
+        onClick={() => {
+          getListName();
+          handleClick();
+        }}
+      >
+        {name}
+      </h3>
+    </div>
+  );
 }
-
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,87 +1,89 @@
+/* eslint-disable no-unused-vars */
 // Page.js
-import { useEffect, useState } from 'react';
-import Checklist from './CheckList';
-import Lists from './Lists';
-import {db} from "../firebase-config.js";
-import {
-  
-  collection,
- getDocs
-} from "firebase/firestore";
+import { useEffect, useState } from "react";
+import Checklist from "./CheckList";
+import Lists from "./Lists";
+import { db } from "../firebase-config.js";
+import { collection, getDocs } from "firebase/firestore";
 
 export default function Page() {
-    // console.log(db)
+  // console.log(db)
 
-      const checklistsRef = collection(db, "Checklists");
+  const checklistsRef = collection(db, "Checklists");
+// eslint-disable-next-line no-unused-vars
 
-const[checklistsData, setChecklistsData] = useState('')
+  const [checklistsData, setChecklistsData] = useState("");
 
-      const getChecklists = async () => {
-        try {
-          const data = await getDocs(checklistsRef);
-          const filteredData = data.docs.map((doc) => ({
-            ...doc.data(),
-          }));
-          setChecklistsData(filteredData);
-          console.log(checklistsData)
-        } catch (err) {
-          console.log(err);
-        }
-      };
+  const getChecklists = async () => {
+    try {
+      const data = await getDocs(checklistsRef);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+      }));
+      setChecklistsData(filteredData);
+      console.log(checklistsData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
+  useEffect(() => {
+    getChecklists();
+  }, []);
 
-useEffect(()=>{
+  const [listName, setListName] = useState("");
+  const [index, setIndex] = useState(0);
+  const [data, setData] = useState([
+    {
+      name: "Workout Log",
+      items: [
+        { id: 1, text: "Exercise 1", checked: false },
+        { id: 2, text: "Exercise 2", checked: false },
+      ],
+    },
+    {
+      name: "Groceries",
+      items: [
+        { id: 1, text: "Milk", checked: false },
+        { id: 2, text: "Bread", checked: false },
+      ],
+    },
+    {
+      name: "To-Do List",
+      items: [
+        { id: 1, text: "Task 1", checked: false },
+        { id: 2, text: "Task 2", checked: false },
+      ],
+    },
+  ]);
 
-    getChecklists()
-},[0])
+  const [items, setItems] = useState([data[index].items]);
 
-    const [listName, setListName] = useState('');
-    const[index, setIndex] = useState(0)
-    const [data, setData] = useState([
-        {
-            name: 'Workout Log',
-            items: [
-                { id: 1, text: 'Exercise 1', checked: false },
-                { id: 2, text: 'Exercise 2', checked: false }
-            ]
-        },
-        {
-            name: 'Groceries',
-            items: [
-                { id: 1, text: 'Milk', checked: false },
-                { id: 2, text: 'Bread', checked: false }
-            ]
-        },
-        {
-            name: 'To-Do List',
-            items: [
-                { id: 1, text: 'Task 1', checked: false },
-                { id: 2, text: 'Task 2', checked: false },
-            ]
-        }
-    ]);
+  const changeItems = (index) => setItems(data[index].items);
 
-
-    const [items, setItems] = useState([data[index].items]);
-
-    const changeItems = (index) => setItems(data[index].items)
-
-    return (
-        <div className="flex justify-center items-center h-full">
-            <div className="flex w-1/2 my-20 h-96 shadow-lg">
-
-                {/* left */}
-                <Lists setListName={setListName} 
-                data={data} setData={setData} 
-                    setIndex={setIndex} index={index} changeItems={changeItems}/>
-                {/* right */}
-                <Checklist listName={listName} 
-                data={data} setData={setData} 
-                    setIndex={setIndex} index={index}
-                    items={items}
-                    setItems={setItems}
-                    />
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="flex w-1/2 my-20 h-96 shadow-lg">
+        {/* left */}
+        <Lists
+          setListName={setListName}
+          data={data}
+          setData={setData}
+          setIndex={setIndex}
+          index={index}
+          changeItems={changeItems}
+        />
+        {/* right */}
+        <Checklist
+          listName={listName}
+          data={data}
+          setData={setData}
+          setIndex={setIndex}
+          index={index}
+          items={items}
+          setItems={setItems}
+        />
+      </div>
+    </div>
+  );
 }
