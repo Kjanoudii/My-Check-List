71ca1ac243f344f597fabeb1c39d5fbd8fb2fe17
Kjanoudii <janoudikhaled@gmail.com>
2024-01-16 18:19:55 +0200

    functionality done

--- src/components/CheckListItem.jsx
+++ src/components/CheckListItem.jsx
@@ -1,25 +1,30 @@
 export default function CheckListItem(prop) {
   const { name, handleCheckboxChange, checked, id, index } = prop;
   return (
-    <li key={index} className={`mt-2 `}>
+    <li key={index} className={`mt-2`}>
       <label className="flex items-center">
         <input
           type="checkbox"
           checked={checked}
           onChange={() => handleCheckboxChange(id)}
-          className="mr-2"
+          className="hidden" // Hide the default checkbox
         />
-        <span>{name}</span>
+        <span
+          className={`checkbox-label ${checked ? "line-through" : ""}`}
+          style={{ cursor: "pointer" }}
+        >
+          {name}
+        </span>
       </label>
     </li>
-
-    // <li
-    //   id={`item-${id}`}
-    //   className={`mt-2 ${checked ? "line-through" : ""}`}
-    //   onClick={() => handleCheckboxChange(id)}
-    //   style={{ cursor: "pointer" }}
-    // >
-    //   {name}
-    // </li>
   );
 }
+
+// <li
+//   id={`item-${id}`}
+//   className={`mt-2 ${checked ? "line-through" : ""}`}
+//   onClick={() => handleCheckboxChange(id)}
+//   style={{ cursor: "pointer" }}
+// >
+//   {name}
+// </li>
